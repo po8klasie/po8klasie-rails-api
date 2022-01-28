f507fa6bbb024958c86313fe822967bc971bac3b
Paweł Biegun <biegunpawel900@gmail.com>
2022-01-28 16:24:29 +0100

    rubocop formatting

--- app/jobs/create_institution_records_job.rb
+++ app/jobs/create_institution_records_job.rb
@@ -6,12 +6,11 @@ class CreateInstitutionRecordsJob < ApplicationJob
   queue_as :default
 
   def perform(institution_type_id)
-
-    i = 1 
-    while true
+    i = 1
+    loop do
       rspo_api_uri = URI("http://194.54.26.132/api/placowki/?page=#{i}&typ_podmiotu_id=#{institution_type_id}")
       request = Net::HTTP::Get.new(rspo_api_uri)
-      request['accept'] = 'application/json' 
+      request['accept'] = 'application/json'
       response = Net::HTTP.start(rspo_api_uri.hostname, rspo_api_uri.port) do |http|
         http.request(request)
       end
@@ -19,34 +18,30 @@ class CreateInstitutionRecordsJob < ApplicationJob
       raw_institutions = JSON.parse(response.body)
 
       # the records are sent in pages of 100
-      if raw_institutions == []
-        break 
-      end
+      break if raw_institutions == []
+
       i += 1
 
       batch_insert_institutions = []
       raw_institutions.each do |raw_institution|
         institution = {}
-        institution["institution_type_id"] = raw_institution['typ']['id']
-        institution["name"] = raw_institution['nazwa']
-        institution["rspo_institution_id"] = raw_institution['numerRspo']
-        if raw_institution['statusPublicznoPrawny']['nazwa'] == 'publiczna'
-          institution["public"] = true 
-        else  
-          institution["public"] = false 
-        end
-        institution["latitude"] = raw_institution['geolokalizacja']['latitude'].to_f
-        institution["longitude"] = raw_institution['geolokalizacja']['longitude'].to_f
-        institution["website"] = raw_institution['stronaInternetowa']
-        institution["email"] = raw_institution['email']
+        institution['institution_type_id'] = raw_institution['typ']['id']
+        institution['name'] = raw_institution['nazwa']
+        institution['rspo_institution_id'] = raw_institution['numerRspo']
+        institution['public'] = raw_institution['statusPublicznoPrawny']['nazwa'] == 'publiczna'
+        institution['latitude'] = raw_institution['geolokalizacja']['latitude'].to_f
+        institution['longitude'] = raw_institution['geolokalizacja']['longitude'].to_f
+        institution['website'] = raw_institution['stronaInternetowa']
+        institution['email'] = raw_institution['email']
         # rubocop:disable Layout/LineLength
-        institution["address"] = "#{raw_institution['adresDoKorespondecjiMiejscowosc']} #{raw_institution['adresDoKorespondecjiUlica']} #{raw_institution['adresDoKorespondecjiNumerBudynku']}/#{raw_institution['adresDoKorespondecjiNumerLokalu']} #{raw_institution['adresDoKorespondecjiKodPocztowy']}"
+        institution['address'] = "#{raw_institution['adresDoKorespondecjiMiejscowosc']} #{raw_institution['adresDoKorespondecjiUlica']} #{raw_institution['adresDoKorespondecjiNumerBudynku']}/#{raw_institution['adresDoKorespondecjiNumerLokalu']} #{raw_institution['adresDoKorespondecjiKodPocztowy']}"
         # rubocop:enable Layout/LineLength
         batch_insert_institutions << institution
       end
 
+      # rubocop:disable Rails/SkipsModelValidations
       Institution.insert_all(batch_insert_institutions)
-    end 
+      # rubocop:enable Rails/SkipsModelValidations
+    end
   end
 end
-
--- app/jobs/enque_parallel_institution_create_job.rb
+++ app/jobs/enque_parallel_institution_create_job.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class EnqueParallelInstitutionCreateJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*_args)
     if Institution.count != 0
       Rails.logger.debug 'To execute this job there can be no Institution records in the database'
-      puts 'To execute this job there can be no Institution records in the database'
+      Rails.logger.debug 'To execute this job there can be no Institution records in the database'
       return
     end
 
     relevant_institution_types_ids = [93, 94, 45, 96, 14, 15, 17, 15, 27, 26, 20, 16]
-    
-    for id in relevant_institution_types_ids 
+
+    relevant_institution_types_ids.each do |id|
       CreateInstitutionRecordsJob.perform_later(id)
     end
   end
