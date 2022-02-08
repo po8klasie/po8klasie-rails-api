94089d5883ffec7544fd207c0c19416951714efc
Paweł Biegun <biegunpawel900@gmail.com>
2022-02-08 19:54:15 +0100

    fix institution integration spec

--- spec/integration/institutions_spec.rb
+++ spec/integration/institutions_spec.rb
@@ -10,9 +10,18 @@ describe 'Institutions API' do
       parameter name: :page, in: :query, type: :string, default: '1', description: 'Page number'
       parameter name: :page_size, in: :query, type: :string, default: '10', description: 'Page size'
 
+      before do 
+        type = create(:institution_type)
+        #create 10 institutions with institution_type: type
+        create_list(:institution, 10, institution_type: type)
+      end
+
+      let(:page) {"2"}
+      let(:page_size) {"3"}
+
       response '200', 'blog found' do
         run_test! do |response|
-          expect(JSON.parse(response.body).size).to equal(10)
+          expect(JSON.parse(response.body).size).to equal(3)
         end
       end
     end
