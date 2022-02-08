3260ac1f4242277fa323f3f9330996ac14ec4025
Paweł Biegun <biegunpawel900@gmail.com>
2022-02-08 23:35:21 +0100

    allow for real http requests in some of the tests when using webmock in others

--- spec/jobs/create_institution_records_job_spec.rb
+++ spec/jobs/create_institution_records_job_spec.rb
@@ -5,9 +5,11 @@ require 'rails_helper'
 RSpec.describe CreateInstitutionRecordsJob, type: :job do
   describe 'after enqueuing the job' do
     it 'adds the institutions from the RSPO API to the database from a given type' do
+      WebMock.allow_net_connect!
       CreateInstitutionTypesJob.new.perform
       CreateInstitutionRecordsJob.new.perform(93)
       expect(Institution.where(rspo_institution_type_id: 93).count).to be_positive
+      WebMock.disable_net_connect!
     end
   end
 end
--- spec/jobs/create_institution_types_job_spec.rb
+++ spec/jobs/create_institution_types_job_spec.rb
@@ -5,9 +5,11 @@ require 'rails_helper'
 RSpec.describe CreateInstitutionTypesJob, type: :job do
   describe 'When enquiuing a CreateInstitutionTypesJob' do
     it 'adds all institution types to the database if there are no InstitutionType records' do
+      WebMock.allow_net_connect!
       CreateInstitutionTypesJob.new.perform
       # if the number of instituion types in the api were to change we want this test to fail
       expect(InstitutionType.count).to eq(58)
+      WebMock.disable_net_connect!
     end
 
     it "doesn't do anything when records are present" do
