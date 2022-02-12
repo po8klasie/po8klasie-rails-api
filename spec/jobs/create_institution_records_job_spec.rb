2141e9f647ab9aa028b909eb15ae9a8cedecb044
Paweł Biegun <biegunpawel900@gmail.com>
2022-02-12 09:12:06 +0100

    add extra test for CreateInstitutionRecordsJob

--- spec/jobs/create_institution_records_job_spec.rb
+++ spec/jobs/create_institution_records_job_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe CreateInstitutionRecordsJob, type: :job do
     it 'adds the institutions from the RSPO API to the database from a given type' do
       WebMock.allow_net_connect!
       CreateInstitutionTypesJob.new.perform
-      CreateInstitutionRecordsJob.new.perform(93)
-      expect(Institution.where(rspo_institution_type_id: 93).count).to be_positive
+      CreateInstitutionRecordsJob.new.perform(27)
+      expect(Institution.where(rspo_institution_type_id: 27).count).to eq(62)
+      expect(Institution.where(rspo_institution_type_id: 27).map(&:rspo_institution_id).uniq.count).to eq(62)
       WebMock.disable_net_connect!
     end
   end
