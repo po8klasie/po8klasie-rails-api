fe773c927a74e16fc362b69b2b5a3865389bb032
Paweł Biegun <biegunpawel900@gmail.com>
2022-04-13 14:10:08 +0200

    rubocop refactoring

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -4,8 +4,8 @@ class Institution < ApplicationRecord
   belongs_to :institution_type
 
   include PgSearch::Model
-  pg_search_scope :search_by_name, against: :name, :using => :dmetaphone
-  pg_search_scope :search_by_area, against: %i[county municipality town], :using => :dmetaphone
+  pg_search_scope :search_by_name, against: :name, using: :dmetaphone
+  pg_search_scope :search_by_area, against: %i[county municipality town], using: :dmetaphone
 
   def address
     "#{town} #{street} #{building_no}/#{apartment_no} #{zip_code}"
