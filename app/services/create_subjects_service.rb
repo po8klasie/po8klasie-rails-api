40b92e15d36b94f0420f723fd823aafc2f7ba049
Pawel Biegun <bigunpawel900@gmail.com>
2022-05-02 16:23:07 +0200

    rewrite and test CreateSubjectsService

--- app/services/create_subjects_service.rb
+++ app/services/create_subjects_service.rb
@@ -3,7 +3,37 @@ class CreateSubjectsService < ApplicationService
     def call  
         #These are the only extended subject fields can be used in subject_sets
         #If there are new subjects that are not in this list it should be updated   
-        subject_names = ["Polski", "Matematyka", "Fizyka", "Chemia", "Geografia", "Historia", "WOS", "Informatyka", "Biologia", "Sztuka", "Dziennikarstwo", "Prawo", "Medycyna", "Nauki ścisłe", "Ekonomia", "Zarządzanie", "Angielski", "Hiszpański", "Niemiecki", "Historia Sztuki", "Włoski", "Rosyjski", "Francuski", "Gotowanie", "Muzyka", "Technik", "Antyk", "Fotografia", "Złotnik"]
+        subject_names = [
+            "Polski", 
+            "Matematyka", 
+            "Fizyka", 
+            "Chemia", 
+            "Geografia", 
+            "Historia", 
+            "WOS", 
+            "Informatyka", 
+            "Biologia", 
+            "Sztuka", 
+            "Dziennikarstwo", 
+            "Prawo", 
+            "Medycyna", 
+            "Nauki ścisłe", 
+            "Ekonomia", 
+            "Zarządzanie", 
+            "Angielski", 
+            "Hiszpański", 
+            "Niemiecki", 
+            "Historia Sztuki", 
+            "Włoski", 
+            "Rosyjski", 
+            "Francuski", 
+            "Gotowanie", 
+            "Muzyka", 
+            "Technik", 
+            "Antyk", 
+            "Fotografia", 
+            "Złotnik"
+        ]
 
         subject_names.each do |subject_name|
             Subject.create(name: subject_name)
